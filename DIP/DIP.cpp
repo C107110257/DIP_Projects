d689ccfda83239b24f72cf864807695f7f2c8908
ken <ken33472225@gmail.com>
2026-02-10 14:35:35 +0800

    Update image loading and project file includes
    Replaced OpenCV/iostream headers with custom myHist.h in DIP.cpp, renamed image variable, and added error handling for image loading. Updated DIP.vcxproj to include myHist source/header files and new image assets.

--- DIP/DIP.cpp
+++ DIP/DIP.cpp
@@ -1,24 +1,28 @@
 
-#include <iostream>
-#include <opencv2\opencv.hpp>
+#include "Course_Lib//myHist.h"
+
 int main()
 {
-    cv::Mat mat0 = cv::imread("images/lena.bmp", 0);
+    cv::Mat m_in = cv::imread("images//lena.bmp", 0);
     cv::Mat mat1, mat2;
-    int ROW = mat0.rows;
-    int COL = mat0.cols;
+    if(m_in.empty()) {
+        std::cout << "Could not open or find the image" << std::endl;
+        return -1;
+	}
+    int ROW = m_in.rows;
+    int COL = m_in.cols;
     mat1.create(ROW, COL, CV_8UC1);
     mat2.create(ROW, COL, CV_8UC1);
     //------------------------------------------------------------------------------------------
     for (int r = 0; r < ROW; r++)
         for (int c = 0; c < COL; c++) {
-            uchar v = mat0.at<uchar>(r, c);
+            uchar v = m_in.at<uchar>(r, c);
             mat1.at<uchar>(ROW - r - 1, c) = v;
             mat2.at<uchar>(r, COL - c - 1) = v;
         }
     //------------------------------------------------------------------------------------------
     cv::namedWindow("Lena_gray_level", cv::WINDOW_AUTOSIZE);
-    cv::imshow("Lena_gray_level", mat0);
+    cv::imshow("Lena_gray_level", m_in);
     //------------------------------------------------------------------------------------------
     cv::namedWindow("Lena_upside_down", cv::WINDOW_AUTOSIZE);
     cv::imshow("Lena_upside_down", mat1);
